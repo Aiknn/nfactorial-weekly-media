32d210e2af8f4407b3ef343d37d27bedd64ae7bd
Yerlan Nurlibayev <ynurlibayev@outlook.com>
2023-01-29 12:42:36 +0600

    add images

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
 
+const postsUrl = 'https://jsonplaceholder.typicode.com/posts';
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-.then(response => response.json())
-.then(json => console.log(json))
+getData(postsUrl, 7);
 
-// function createNode(element) {
-//     return document.createElement(element);
-// }
+function getData(url, number){
+    fetch(url)
+    .then(promise => promise.json())
+    .then(function(data){
+        data.splice(0, number).forEach(item=>{
+            console.log(item);
+            createPost(item);
+        })
+    })
+    .catch(e=>console.log(e))
+}
 
-// function append(parent, el) {
-//   return parent.appendChild(el);
-// }
+function createPost(item){
+    let post = `
+        <div class="post">
+        <div class="row">
+            <div class="col">
+                <div class="author-info">
+                    <img src="./images/author.png" alt="autor">
+                    <span class="author">Authors name </span>
+                    <span class="author-light">in </span>
+                    <span class="author">Topics Name · </span>
+                    <span class="author-light">7 july</span>
+                </div>
+                <p class="topic"> </p>
+                <p class="text"> </p>
+            </div>
+            <div class="col-4">
+                <img alt="post_image" src="./images/8.jpg" class="rounded mx-auto d-block img-fluid">
+            </div>
+        </div>
+        <div class="extra">
+            <div class="extra-text">
+                <button class="button">Java Script</button>
+                <p class="author-light centered">12 min read &nbsp; · &nbsp; Selected for you</p>
+            </div>
+            <div class="extra-boxes">
+                <div class="empty-box"></div>
+                <div class="empty-box"></div>
+                <div class="empty-box"></div>
+            </div>
+        </div>
+        </div>
+        <hr>
+        `
 
-// const ul = document.getElementById('authors');
-// const url = 'https://randomuser.me/api/?results=10';
+    const posts = document.getElementById("posts");
 
-// fetch(url)
-// .then((resp) => resp.json())
-// .then(function(data) {
-//   let authors = data.results;
-//   return authors.map(function(author) {
-//     let li = createNode('li');
-//     let img = createNode('img');
-//     let span = createNode('span');
-//     img.src = author.picture.medium;
-//     span.innerHTML = `${author.name.first} ${author.name.last}`;
-//     append(li, img);
-//     append(li, span);
-//     append(ul, li);
-//   })
-// })
-// .catch(function(error) {
-//   console.log(error);
-// });
+    let newPost = post.replace('<p class="topic">', `<p class="topic">${item.title}`);
+    newPost = newPost.replace('<p class="text">', `<p class="text">${item.body}`);
+    newPost = newPost.replace('<img alt="post_image" src="./images/8.jpg"', `<img alt="post_image" src="./images/${item.id}.jpg"`)
+
+    posts.innerHTML += newPost;
+};
